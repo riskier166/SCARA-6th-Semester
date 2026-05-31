86aa4b3b4c77bf1a59842a7e3fdacdf21d434311
riskier166 <richi0914@outlook.com>
2026-05-30 23:24:15 -0600

    GG

--- src/definitions.h
+++ src/definitions.h
@@ -19,8 +19,10 @@ PID control;
 float gains[3];
 float reference=0;
 int mode;
-
 float measurement;float error; float u;
+
+// Help variables 
+float speed = 1000.0,getSpeed,getAngle; 
 
 enum MODE{
     NOTHING = 0,
@@ -31,11 +33,20 @@ enum MODE{
 MODE current_mode;
 
 ///////////////// ELBOW DCM STUFF ////////////////////////////////////
-uint8_t PWM_PIN[2] = {32, 33};uint8_t PWMCH[2] = {0, 1}; //PWM Channels
-HBridge MOTOR_PWM; //HBridge class instance
+uint8_t ElbowPIN[2] = {32, 33};uint8_t ElbowPWMCH[2] = {0, 1}; //PWM Channels
+HBridge ElbowDCM; //HBridge class instance
 //Quadrature Encoder Stuff
-QuadratureEncoder encoder;
-uint8_t EncIN[] = {36, 39}; const float degrees_per_edge = 0.36437;
+QuadratureEncoder ElbowEncoder;
+uint8_t ElbowEncPIN[] = {39,36}; const float degrees_per_edge = 0.36437;
+// Calibration Limit Switch Stuff 
+SimpleGPIO Calibration; const uint8_t CalibPin = 27;
+
+///////////////// WRIST DCM STUFF ////////////////////////////////////
+uint8_t WristPIN[2] = {25, 26};uint8_t WristPWMCH[2] = {2, 3}; //PWM Channels
+HBridge WristDCM; //HBridge class instance
+//Quadrature Encoder Stuff
+QuadratureEncoder WristEncoder;
+uint8_t WristEncPIN[] = {34,35}; 
 
 ///////////////// STEPPER HIP STUFF //////////////////////////////////
 SimplePWM Step1;
@@ -43,7 +54,7 @@ SimpleGPIO Dir1;
 const uint8_t step_pin1 = 18, dir_pin1 = 19,step1_channel = 4;
 TimerConfig stepper1_config{
     .timer = LEDC_TIMER_1,
-    .frequency = 1000, // 400Hz
+    .frequency = 1000, // 1kHz
     .bit_resolution = LEDC_TIMER_10_BIT,
     .mode = LEDC_HIGH_SPEED_MODE};
 // Absolute encoder (12c) stuff
@@ -53,24 +64,24 @@ AS5600 AbsEnc(&i2c);
 ///////////////// UP-DOWN Stepper stuff //////////////////////////////
 SimplePWM Step2;
 SimpleGPIO Dir2;
-const uint8_t step_pin2 = 33, dir_pin2 = 32,step2_channel = 1;
+const uint8_t step_pin2 = 16, dir_pin2 = 17,step2_channel = 5;
 TimerConfig stepper2_config{
     .timer = LEDC_TIMER_0,
     .frequency = 1000, // 400Hz
     .bit_resolution = LEDC_TIMER_10_BIT,
     .mode = LEDC_HIGH_SPEED_MODE};
-// Help vars
-int frequency = 1000, direction2, on_off_2; 
-// Limit switches stuff
-SimpleGPIO down_LS, up_LS;
-int8_t DOWN_PIN = 35, UP_PIN = 34;
+// Limit Up Down stuff
+SimpleGPIO UpDown_LS; const uint8_t UpDownPin = 4;
+
+///////////////////// GRIPPER  stuff //////////////////////////////////
+SimpleGPIO Gripper; const uint8_t GripperPin = 13;
 
 // while Timer Stuff
 SimpleTimer timer;
 bool flag = false;
 uint64_t dt_us = 10000; // 10 ms = 10000 us
 
-//UART Stuff
+// UART Stuff
 SimpleUART uart(115200); //UART class
 char buffer[30];int message_length;
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,18 @@ extern "C" void app_main()
     {
         if (timer.interruptAvailable())
         {
+            // WristDCM.setSpeed(speed);
+            // getSpeed = WristEncoder.getSpeed();
+            // getAngle = WristEncoder.getAngle();
+
+            // message_length = uart.available();
+            // if (message_length)
+            // {
+            //     uart.read(buffer, message_length); // Echo back
+            //     sscanf(buffer, "%f", &speed);
+            // }
+            // printf("Speed: %f,%f,%f\n", speed, getSpeed, getAngle);
+
             Step1.setDuty(80);
             Dir1.set(1);
             if (AbsEnc.update() == ESP_OK)
@@ -18,16 +30,23 @@ extern "C" void app_main()
                    AbsEnc.getAngularVelocityDegS());
             }
         }
-    }
+    }  
 }
 
 void setups()
 {
-    // DC Motor setup
-    MOTOR_PWM.setup(PWM_PIN, PWMCH);
+    //// Elbow DCM setup
+    ElbowDCM.setup(ElbowPIN, ElbowPWMCH);
+    /// Quadrature encoder setup
+    ElbowEncoder.setup(ElbowEncPIN, degrees_per_edge);
+    control.setup(gains, dt_us / 1000000.0f); //PID 
+    // Calibration Limit Switch setup 
+    Calibration.setup(CalibPin,GPI);
+
+    // Wrist DCM setup
+    WristDCM.setup(WristPIN, WristPWMCH);
     // Quadrature encoder setup
-    encoder.setup(EncIN, degrees_per_edge);
-    control.setup(gains, dt_us / 1000000.0f);
+    WristEncoder.setup(WristEncPIN, degrees_per_edge);
 
     // While timer Setup
     timer.setup(timerISR, "MainTimer");timer.startPeriodic(dt_us);
@@ -35,8 +54,17 @@ void setups()
     // Stepper HIP setup 
     Step1.setup(step_pin1, step1_channel, &stepper1_config);
     Dir1.setup(dir_pin1, GPO);
-    // Absollute Encoder Setup
+    /// Absolute Encoder Setup
     i2c.init(); AbsEnc.init();
+
+    // Stepper UpDown Setup 
+    Step2.setup(step_pin2, step2_channel, &stepper2_config);
+    Dir2.setup(dir_pin2, GPO);
+    /// UpDown Limit Switch Setup 
+    UpDown_LS.setup(UpDownPin,GPI);
+
+    // Gripper Setup 
+    Gripper.setup(GripperPin,GPO);
 }
 
 static void IRAM_ATTR timerISR(void *arg)
